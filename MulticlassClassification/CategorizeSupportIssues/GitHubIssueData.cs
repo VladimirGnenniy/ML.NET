597acc644d3662fee9b18152f0033b3a26465f61
Vladimir <vladimirgnenniy@gmail.com>
2020-12-04 17:54:54 +0200

    Add multiclass classification project,

---
+++ MulticlassClassification/CategorizeSupportIssues/GitHubIssueData.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.ML.Data;
+namespace CategorizeSupportIssues
+{
+    public class GitHubIssue
+    {
+        [LoadColumn(0)]
+        public string ID { get; set; }
+        [LoadColumn(1)]
+        public string Area { get; set; }
+        [LoadColumn(2)]
+        public string Title { get; set; }
+        [LoadColumn(3)]
+        public string Description { get; set; }
+    }
+
+    public class IssuePrediction
+    {
+        [ColumnName("PredictedLabel")]
+        public string Area;
+    }
+}
